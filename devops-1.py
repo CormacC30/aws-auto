89266861dc01ac7634c991f31ededd9c542ad94f
CormacC30 <cormaccostello2@gmail.com>
2024-03-01 12:00:29 +0000

    6 random characters generated for bucket name

--- devops-1.py
+++ devops-1.py
@@ -3,6 +3,8 @@ import webbrowser
 import time
 import datetime
 import json
+import random
+import string
 import subprocess
 ec2 = boto3.resource('ec2')
 s = [] # the list of instance names
@@ -86,15 +88,22 @@ webbrowser.open_new_tab(f"http://{ip_address}")
 s3 = boto3.resource("s3")
 s3client = boto3.client("s3")
 
-new_bucket_name = f'demo-aws-bucket-{formatted_time}' # unique bucket name with the current date time,
+def get_random_string(length):
+    characters = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
 
+bucket_id_string = get_random_string(6)
+
+bucket_name = f'ccostello-{bucket_id_string}' # unique bucket name with 6 random chars
+
+# create new bucket 
 try:
-    response = s3.create_bucket(Bucket=new_bucket_name)
+    response = s3.create_bucket(Bucket=bucket_name)
     print(response)
 except Exception as error:
     print(error)
 
-s3client.delete_public_access_block(Bucket=new_bucket_name)
+s3client.delete_public_access_block(Bucket=bucket_name)
 # set public read access - bucket policy
 bucket_policy = {
                 "Version": "2012-10-17",
@@ -104,21 +113,21 @@ bucket_policy = {
                     "Effect": "Allow",
                     "Principal": "*",
                     "Action": ["s3:GetObject"],
-                    "Resource": f"arn:aws:s3:::{new_bucket_name}/*"
+                    "Resource": f"arn:aws:s3:::{bucket_name}/*"
                 }
                 ]
 }
-s3.Bucket(new_bucket_name).Policy().put(Policy=json.dumps(bucket_policy)) 
+s3.Bucket(bucket_name).Policy().put(Policy=json.dumps(bucket_policy)) 
    
 website_configuration = {
     'ErrorDocument': {'Key': 'error.html'},
     'IndexDocument': {'Suffix': 'index.html'},
 }
 
-bucket_website = s3.BucketWebsite(f'{new_bucket_name}')   
+bucket_website = s3.BucketWebsite(f'{bucket_name}')   
 
 bucket_website.put(WebsiteConfiguration=website_configuration)
-print(f"bucket name: {new_bucket_name}")
+print(f"bucket name: {bucket_name}")
 print("Upload an index.html file to test it works!")
 
 cmd = f"scp -o StrictHostKeyChecking=no -i ~/DevOps/HDip-2024.pem ec2-user@{ip_address}:/var/www/html/index.html ."
@@ -130,7 +139,7 @@ print (result.returncode)
 object_name = 'index.html'
 
 try:
-    response = s3.Object(new_bucket_name, object_name).put(Body=open(object_name, 'rb'),ContentType='text/html')
+    response = s3.Object(bucket_name, object_name).put(Body=open(object_name, 'rb'),ContentType='text/html')
     print (response)
 except Exception as error:
     print (error)
@@ -146,7 +155,7 @@ subprocess.run(command, shell=True)
 image_object_name = 'logo.jpg'
 
 try:
-    response = s3.Object(new_bucket_name, image_object_name).put(Body=open(image_object_name, 'rb'),ContentType='image.jpeg')
+    response = s3.Object(bucket_name, image_object_name).put(Body=open(image_object_name, 'rb'),ContentType='image.jpeg')
     print (response)
 except Exception as error:
     print (error)
